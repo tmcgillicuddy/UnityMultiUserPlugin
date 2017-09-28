96effdb693e243298ef6cbf9aff6c67f12bcb245
Thomas McGillicuddy <thomas.mcgillicuddy@mymail.champlain.edu>
2017-09-28 12:23:03 -0400

    Added base UI files and plugin information

---
+++ EGP-405 Networking Final/Assets/Multiuser Plugin/Multiuser Plugin.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+[InitializeOnLoad]
+public class MultiuserPlugin
+{
+    public static bool mConnected;
+    public static int mPortNum = 6666;
+    public static string mIP = "127.07.04";
+
+    static MultiuserPlugin()
+    {
+        EditorApplication.update += Update;
+        mConnected = false;
+
+    }
+
+    //Update Loop
+    static void Update()
+    {
+        if(mConnected)
+        {
+            Debug.Log("Updating");
+        }
+    }
+}
---
+++ EGP-405 Networking Final/Assets/Multiuser Plugin/Multiuser_Editor_Window.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class Multiuser_Editor_Window : EditorWindow
+{
+    [MenuItem("Window/Multiuser Network")]
+    static void init()
+    {
+        Multiuser_Editor_Window window = (Multiuser_Editor_Window)GetWindow(typeof(Multiuser_Editor_Window));
+        window.Show();
+    }
+
+    private void OnGUI()
+    {
+        EditorGUILayout.BeginHorizontal();
+
+        GUILayout.Label("Connection Port Number");
+        MultiuserPlugin.mPortNum = EditorGUILayout.IntField(MultiuserPlugin.mPortNum);
+
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+
+        GUILayout.Label("Connection IP");
+        MultiuserPlugin.mIP = EditorGUILayout.TextField(MultiuserPlugin.mIP);
+
+        EditorGUILayout.EndHorizontal();
+
+        MultiuserPlugin.mConnected = GUILayout.Toggle(MultiuserPlugin.mConnected, "Run");
+    }
+}
