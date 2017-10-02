13c679c3267fa60a60097ff247e8705e69237686
EMCLAP-04\emcuser <thomas.mcgillicuddy@mymail.champlain.edi>
2017-10-02 09:30:13 -0400

    laptop front end push

--- EGP-405 Networking Final/Assets/Multiuser Plugin/Multiuser Plugin.cs
+++ EGP-405 Networking Final/Assets/Multiuser Plugin/Multiuser Plugin.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 [InitializeOnLoad]
 public class MultiuserPlugin
 {
-    public static bool mConnected;  //If the system is running;
+    public static bool mConnected, mIsPaused;  //If the system is running;
     public static int mPortNum = 6666;      //Which port to connect through
     public static string mIP = "127.07.04"; //Which IP to connect to
     public static float syncInterval = 0f;   //How often system should sync
@@ -38,7 +38,7 @@ public class MultiuserPlugin
     //Update Loop
     static void Update()
     {
-        if (!Application.isPlaying)
+        if (!Application.isPlaying && !mIsPaused)
         {
             if (toolMode == mode.EDIT)
             {
@@ -90,33 +90,33 @@ public class MultiuserPlugin
     }
     public static void Sync()
     {
-        GameObject[] allGameobjects = GameObject.FindObjectsOfType<GameObject>();
-        Debug.Log("Syncing");
+            GameObject[] allGameobjects = GameObject.FindObjectsOfType<GameObject>();
+            Debug.Log("Syncing");
 
-        for (int i = 0; i < allGameobjects.Length; ++i) //Checks All objects in scene and 
-        {
-            MarkerFlag objectFlag = allGameobjects[i].GetComponent<MarkerFlag>();
-            if (objectFlag == null)    //If an object doesn't have the marker flag script on it
-            {                                                           //it will be added
-                objectFlag = allGameobjects[i].AddComponent<MarkerFlag>();
-            }
-
-            if (objectFlag.isModified)    //If this object's marker flag has been modified
+            for (int i = 0; i < allGameobjects.Length; ++i) //Checks All objects in scene and 
             {
-                Component[] allComponenets = allGameobjects[i].GetComponents<Component>();
-                for (int j = 0; j < allComponenets.Length; ++j)    //Checks the marked gameobject's componentss
-                {
-                    //THIS IS WHERE WE WILL COMPILE DATA TO SEND
+                MarkerFlag objectFlag = allGameobjects[i].GetComponent<MarkerFlag>();
+                if (objectFlag == null)    //If an object doesn't have the marker flag script on it
+                {                                                           //it will be added
+                    objectFlag = allGameobjects[i].AddComponent<MarkerFlag>();
                 }
-                objectFlag.isModified = false;
-            }
 
-            if (!Selection.Contains(allGameobjects[i]))
-            {
-                objectFlag.isLocked = false;
-            }
+                if (objectFlag.isModified)    //If this object's marker flag has been modified
+                {
+                    Component[] allComponenets = allGameobjects[i].GetComponents<Component>();
+                    for (int j = 0; j < allComponenets.Length; ++j)    //Checks the marked gameobject's componentss
+                    {
+                        //THIS IS WHERE WE WILL COMPILE DATA TO SEND
+                    }
+                    objectFlag.isModified = false;
+                }
 
+                if (!Selection.Contains(allGameobjects[i]))
+                {
+                    objectFlag.isLocked = false;
+                }
+
+            }
         }
     }
 
-}
--- EGP-405 Networking Final/Assets/Multiuser Plugin/Multiuser_Editor_Window.cs
+++ EGP-405 Networking Final/Assets/Multiuser Plugin/Multiuser_Editor_Window.cs
@@ -8,6 +8,9 @@ using System.Runtime.Serialization.Formatters.Binary;
 public class Multiuser_Editor_Window : EditorWindow
 {
 
+    bool test;
+
+
     [MenuItem("Window/Multiuser Network")]
     static void init()
     {
@@ -17,35 +20,68 @@ public class Multiuser_Editor_Window : EditorWindow
 
     private void OnGUI()
     {
-        EditorGUILayout.BeginHorizontal();
 
-        GUILayout.Label("Connection Port Number");
-        MultiuserPlugin.mPortNum = EditorGUILayout.IntField(MultiuserPlugin.mPortNum);
-
-        EditorGUILayout.EndHorizontal();
-
-        EditorGUILayout.BeginHorizontal();
-
-        GUILayout.Label("Connection IP");
-        MultiuserPlugin.mIP = EditorGUILayout.TextField(MultiuserPlugin.mIP);
-
-        EditorGUILayout.EndHorizontal();
-
-        MultiuserPlugin.toolMode = (MultiuserPlugin.mode)EditorGUILayout.EnumPopup("Mode:", MultiuserPlugin.toolMode);
-
-
-        EditorGUILayout.BeginHorizontal();
-
-        GUILayout.Label("Sync Interval (0 for realtime)");
-        MultiuserPlugin.syncInterval = EditorGUILayout.FloatField(MultiuserPlugin.syncInterval);
-        GUILayout.Label("seconds");
-        EditorGUILayout.EndHorizontal();
-
-        MultiuserPlugin.mConnected = GUILayout.Toggle(MultiuserPlugin.mConnected, "Auto Run");
-
-        if(GUILayout.Button("Manual Sync"))
+        if (!MultiuserPlugin.mConnected)
         {
-            MultiuserPlugin.Sync();
+            EditorGUILayout.BeginHorizontal();
+
+            GUILayout.Label("Connection Port Number");
+            MultiuserPlugin.mPortNum = EditorGUILayout.IntField(MultiuserPlugin.mPortNum);
+
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.BeginHorizontal();
+
+            GUILayout.Label("Connection IP");
+            MultiuserPlugin.mIP = EditorGUILayout.TextField(MultiuserPlugin.mIP);
+
+            EditorGUILayout.EndHorizontal();
+
+            MultiuserPlugin.toolMode = (MultiuserPlugin.mode)EditorGUILayout.EnumPopup("Mode:", MultiuserPlugin.toolMode);
+
+            if (GUILayout.Button("Connect"))
+            {
+                MultiuserPlugin.mConnected = true;
+            }
+        }
+        else
+        {
+            EditorGUILayout.BeginHorizontal();
+
+            GUILayout.Label("Sync Interval (0 for realtime)");
+            MultiuserPlugin.syncInterval = EditorGUILayout.FloatField(MultiuserPlugin.syncInterval);
+            GUILayout.Label("seconds");
+
+            if(MultiuserPlugin.mIsPaused)
+            {
+                if (GUILayout.Button("Unpause"))
+                {
+                    MultiuserPlugin.mIsPaused = false;
+                }
+            }
+            else
+            {
+                if (GUILayout.Button("Pause"))
+                {
+                    MultiuserPlugin.mIsPaused = true;
+                }
+            }
+            EditorGUILayout.EndHorizontal();
+
+
+            EditorGUILayout.BeginHorizontal();
+
+            if (GUILayout.Button("Disconnect"))
+            {
+                MultiuserPlugin.mConnected = false;
+            }
+
+            if (GUILayout.Button("Manual Sync"))
+            {
+                MultiuserPlugin.Sync();
+            }
+            EditorGUILayout.EndHorizontal();
+
         }
     }
 }
