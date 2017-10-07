a28d218c1ac1a421794a320ac1db9e01fce33922
James Smith04 <james.smith04@mymail.champlain.edu>
2017-10-07 14:15:18 -0400

    Successfully serialized transform

--- EGP-405 Networking Final/Assets/Multiuser Plugin/StructScript.cs
+++ EGP-405 Networking Final/Assets/Multiuser Plugin/StructScript.cs
@@ -1,27 +1,80 @@
-﻿using System.Collections;
+﻿using System;
+using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 
 public class StructScript {
 
     public char[] serialize(GameObject obj)
     {
+        List<char> serialized = new List<char>();
         Debug.Log("Checking");
         Component[] comps;
         comps = obj.GetComponents<Component>();
         Debug.Log(comps.Length);
+        GameObject newObj = new GameObject();
         for(int i = 0; i < comps.Length; i++)
         {
-            if(comps[i].GetType() == typeof(UnityEngine.Transform))
+            if (comps[i].GetType() == typeof(UnityEngine.Transform))
             {
                 Debug.Log("Has Transform");
+                UnityEngine.Transform temp = comps[i] as UnityEngine.Transform;
+                Transform serTemp = new Transform();
+                serTemp.pos = temp.position;
+                serTemp.rot = temp.rotation;
+                serTemp.scale = temp.localScale; //Look here for scaling issues
+                string tempString = new string(serTemp.toChar());
+                Debug.Log(tempString);
+                
+            }
+            else if (comps[i].GetType() == typeof(UnityEngine.BoxCollider))
+            {
+                Debug.Log("Has Box Collider");
+            }
+            else if (comps[i].GetType() == typeof(UnityEngine.SphereCollider))
+            {
+                Debug.Log("Has Shpere Collider");
+            }
+            else if (comps[i].GetType() == typeof(UnityEngine.CapsuleCollider))
+            {
+                Debug.Log("Has Capsule Collider");
             }
             else if (comps[i].GetType() == typeof(UnityEngine.Rigidbody))
             {
                 Debug.Log("Has Rigidbody");
             }
+            else if (comps[i].GetType() == typeof(UnityEngine.Camera))
+            {
+                Debug.Log("Has Camera");
+            }
+            else if (comps[i].GetType() == typeof(UnityEngine.MeshFilter))
+            {
+                Debug.Log("Has Mesh Filter");
+            }
+            else
+                Debug.Log(comps[i].GetType());
         }
         return null;
+    }
+    public string vecToString(Vector3 vec)
+    {
+        string temp = "";
+        temp += vec.x;
+        temp += vec.y;
+        temp += vec.z;
+        return temp;
+    }
+
+    public string quatToString(Quaternion qua)
+    {
+        string temp = "";
+        temp += qua.x;
+        temp += qua.y;
+        temp += qua.z;
+        temp += qua.w;
+        return temp;
     }
 
 }
@@ -41,6 +94,8 @@ public class serializedComponent
         MESHFILTER
     }
     typeID id;
+
+    public virtual char[] toChar() { return null; }
 
 }
 
@@ -63,12 +118,27 @@ struct Color
 
 public class Transform : serializedComponent
 {
-    Vector3 pos;
-    Quaternion rot;
-    Vector3 scale;
+    public Vector3 pos;
+    public Quaternion rot;
+    public Vector3 scale;
+    public virtual char[] toChar()
+    {
+        string temp = "";
+        temp += pos.x + " ";
+        temp += pos.y + " ";
+        temp += pos.z + " ";
+        temp += rot.x + " ";
+        temp += rot.y + " ";
+        temp += rot.z + " ";
+        temp += rot.w + " ";
+        temp += scale.x + " ";
+        temp += scale.y + " ";
+        temp += scale.z + " ";
+        return temp.ToCharArray();
+    }
 }
 
-public class BoxCollider : serializedComponent
+/*public class BoxCollider : serializedComponent
 {
     Vector3 center;
     Vector3 size;
@@ -161,6 +231,5 @@ public class Camera : serializedComponent
 public class MeshFilter : serializedComponent
 {
     string fileName;
-}
-
+}*/
 
