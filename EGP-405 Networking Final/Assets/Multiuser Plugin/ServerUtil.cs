20b9834324463a60f2fcce1dac999fbca73285a8
Hamilton <aaron.hamilton@CHAMPLAIN.EDU>
2017-11-06 12:42:45 -0500

    Almost created system of organizing autosaved scenes in different folders by date rather than deleting older ones

--- EGP-405 Networking Final/Assets/Multiuser Plugin/ServerUtil.cs
+++ EGP-405 Networking Final/Assets/Multiuser Plugin/ServerUtil.cs
@@ -19,6 +19,7 @@ using System.IO;
 [InitializeOnLoad]
 public  class ServerUtil {
     static DateTime lastSaveTime = DateTime.Now;
+    public static int folderIteration = 0;
     public static int saveInterval = 2;
     public const int MAX_SAVED_SCENES = 10;
 
@@ -50,9 +51,16 @@ public  class ServerUtil {
 
     public static void saveToNewScene()
     {
-        String newTimestamp = getTimestamp(DateTime.Now);
+        string folderName = "Autosaved Scenes ";
+        string basePath = "Assets/Scenes/";
+        string folderTimestamp = getTimestamp(DateTime.Now, true, false);
+        String newTimestamp = getTimestamp(DateTime.Now, true, true);
+
+        bool isNewDay = isLastFileOld();
+
         if (DateTime.Now >= lastSaveTime)
         {
+            Debug.Log(lastSaveTime);
             Debug.Log("saveToNewScene()::Server side saving");
 
             // Get the new scene name
@@ -71,25 +79,42 @@ public  class ServerUtil {
             EditorSceneManager.MergeScenes(EditorSceneManager.GetSceneByName(oldSceneName), newScene);
             // copied everything from old scene into new scene
 
-            // save new scene
-            EditorSceneManager.SaveScene(newScene, "Assets/Scenes/Autosaved Scenes/" + newSceneName + ".unity", false);
-            // saved new scene
+            Debug.Log(isNewDay);
+
+            if (isNewDay)
+            {
+                Debug.Log("is new day");
+                string guid = AssetDatabase.CreateFolder(basePath, folderName + folderTimestamp);
+                string newPath = AssetDatabase.GUIDToAssetPath(guid);
+                Debug.Log(guid);
+                Debug.Log(newPath);
+                //if (isFolderFull(newPath))
+                {
+                    /*
+                    ++folderIteration;
+                    guid = AssetDatabase.CreateFolder(basePath, folderName + folderTimestamp + "(" + folderIteration + ")");
+                    newPath = AssetDatabase.GUIDToAssetPath(guid);
+                    */
+                }
+                // save new scene
+                EditorSceneManager.SaveScene(newScene, newPath + newSceneName + ".unity", false);
+                // saved new scene
+            }
         }
     }
 
-    public string[] scenes;
     public static void checkTooManyScenes()
     {
         EditorSceneManager.preventCrossSceneReferences = false;
         Debug.Log("checkTooManyScenes()");
-        string path = "Assets/Scenes/Autosaved Scenes/";
+        String path = "Assets/Scenes/Autosaved Scenes/";
 
         DirectoryInfo levelDirectoryPath = new DirectoryInfo(path);
         FileInfo[] scenesInfo = levelDirectoryPath.GetFiles();
 
-        Queue<string> sceneNames = new Queue<string>();
+        Queue<String> sceneNames = new Queue<String>();
 
-        EditorSceneManager.SaveOpenScenes();
+       // EditorSceneManager.SaveOpenScenes();
         Debug.Log(EditorSceneManager.sceneCount);
 
         int i = 0;
@@ -113,11 +138,12 @@ public  class ServerUtil {
 
                 Scene scene = EditorSceneManager.GetSceneByName(earliestScene);
 
-                //Debug.Log(EditorSceneManager.CloseScene(scene, true));
-                //EditorSceneManager.Destroy(scene);
-                EditorSceneManager.OpenScene(path + earliestScene, OpenSceneMode.Single);
-                Debug.Log("Active Scene: " + EditorSceneManager.GetActiveScene().name);
-                Debug.Log("Deleted Scene: " + EditorSceneManager.CloseScene(SceneManager.GetActiveScene(), true));
+                Debug.Log(EditorSceneManager.CloseScene(scene, true));
+                Debug.Log("Opening scene " + EditorSceneManager.OpenScene(path + earliestScene).name);
+                Debug.Log("Current open scene is " + EditorSceneManager.GetActiveScene().name);
+                EditorSceneManager.OpenScene(path + earliestScene);
+
+                Debug.Log(EditorSceneManager.CloseScene(SceneManager.GetSceneByName(earliestScene), true));
 
                 Debug.Log(path + earliestScene + " was Deleted");
             }
@@ -125,8 +151,81 @@ public  class ServerUtil {
         }
     }
 
-    public static String getTimestamp(DateTime val)
+    public static bool isLastFileOld()
     {
-        return val.ToString("yyyy.MM.dd HH.mm.ss");
+        bool val = false;
+
+        string path = "Assets/Scenes/";
+
+        DirectoryInfo levelDirectoryPath = new DirectoryInfo(path);
+
+        DirectoryInfo[] folderInfo = levelDirectoryPath.GetDirectories();
+
+        foreach (DirectoryInfo d in folderInfo)
+        {
+            Debug.Log(d.Name);
+        }
+
+        if (folderInfo.Length == 0)
+            val = true;
+        else
+        {
+            string lastFolderName = folderInfo[folderInfo.Count() - 1].Name;
+            //string lastFolderName = folderInfo[0].Name;
+            Debug.Log(lastFolderName);
+            int i = 0;
+            int numSpaces = 0;
+            Debug.Log(lastFolderName.Length);
+            while (numSpaces < 2)
+            {
+                if (lastFolderName[i] == ' ')
+                    numSpaces++;
+                ++i;
+            }
+
+            Debug.Log(lastFolderName.Substring(i));
+            if (lastFolderName.Substring(i) == getTimestamp(DateTime.Now, true, false))
+                val = false;
+            else
+                val = true;
+        }
+
+        return val;
+    }
+
+    public static bool isFolderFull(string path)
+    {
+        bool val = false;
+
+        DirectoryInfo levelDirectoryPath = new DirectoryInfo(path);
+        DirectoryInfo[] folderInfo = levelDirectoryPath.GetDirectories();
+
+        int i = 0;
+        foreach (DirectoryInfo d in folderInfo)
+        {
+            Debug.Log(d.Name);
+            if (d.Name.Contains(".meta") == false)
+            {
+                i++;
+            }
+        }
+
+        if (i < 10)
+            val = false;
+        else
+            val = true;
+
+        return val;
+
+    }
+
+    public static String getTimestamp(DateTime val, bool date, bool time)
+    {
+        if (date && !time)
+            return val.ToString("yyyy.MM.dd");
+        else if (!date && time)
+            return val.ToString("HH.mm.ss");
+        else
+            return val.ToString("yyyy.MM.dd HH.mm.ss");
     }
 }
