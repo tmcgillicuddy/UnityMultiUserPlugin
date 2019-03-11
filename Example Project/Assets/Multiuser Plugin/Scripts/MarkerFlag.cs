480b0c6b6f0c05a38ec186e6b32e33e0e102e8e8
Thomas McGillicuddy <thomas.mcgillicuddy@mymail.champlain.edu>
2019-03-11 17:30:36 -0400

    removing old example project for class, replacing with clean example project. Also adding folder with just plugin and relevant editor scripts

---
+++ Example Project/Assets/Multiuser Plugin/Scripts/MarkerFlag.cs
@@ -0,0 +1,82 @@
+﻿/*
+EGP 405-01 Final Project 12/7/17
+Aaron Hamilton
+James Smith
+Thomas McGillicuddy
+“We certify that this work is
+entirely our own. The assessor of this project may reproduce this project
+and provide copies to other academic staff, and/or communicate a copy of
+this project to a plagiarism-checking service, which may retain a copy of the
+project on its database.”
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class MarkerFlag : MonoBehaviour {
+     public string id;
+     public bool isModified = false; //Marks if the object needs to be sent over the network
+     public bool isHeld = false; //Marks if YOU'RE holding the object
+     public bool isLocked = false; //Marks if an object is held by another client
+     public string parentID;
+
+    public Vector3 ogPos, ogScale;
+    public Quaternion ogRot;
+
+    private void OnDrawGizmos()
+    {
+        if (isLocked)
+        {
+            Renderer[] childRenderer = GetComponentsInChildren<Renderer>();
+            Bounds tempBounds = getBounds(this.gameObject);
+
+            Gizmos.color = new Color(1, 1, 0, 0.75f);
+            Gizmos.DrawWireCube(tempBounds.center, tempBounds.size);
+        }
+        
+    }
+    Bounds getBounds(GameObject objeto)
+    {
+        Bounds bounds;
+        bounds = getRenderBounds(objeto);
+        if (bounds.extents.x == 0)
+        {
+            bounds = new Bounds(objeto.transform.position, Vector3.zero);
+            Renderer[] childRenderers = GetComponentsInChildren<Renderer>();
+            foreach (Renderer child in childRenderers)
+            {
+                if (child)
+                {
+                    bounds.Encapsulate(child.bounds);
+                }
+                else
+                {
+                    bounds.Encapsulate(getBounds(child.gameObject));
+                }
+            }
+        }
+        return bounds;
+    }
+    Bounds getRenderBounds(GameObject objeto)
+    {
+        Bounds bounds = new Bounds(Vector3.zero, Vector3.zero);
+        Renderer render = objeto.GetComponent<Renderer>();
+        if (render != null)
+        {
+            return render.bounds;
+        }
+        return bounds;
+    }
+    void OnEnable()
+    {
+        hideFlags = HideFlags.HideInInspector;
+       
+    }
+    private void OnDestroy()
+    {
+        MultiuserPlugin.DeleteObject(this);
+    }
+}
---
+++ Example Project/Assets/Multiuser Plugin/Scripts/ServerUtil.cs
@@ -0,0 +1,137 @@
+﻿/*
+EGP 405-01 Final Project 12/7/17
+Aaron Hamilton
+James Smith
+Thomas McGillicuddy
+“We certify that this work is
+entirely our own. The assessor of this project may reproduce this project
+and provide copies to other academic staff, and/or communicate a copy of
+this project to a plagiarism-checking service, which may retain a copy of the
+project on its database.”
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.IO;
+
+/*
+  This is used to run certain fetures only the server has access to
+  These Include:
+  Autosave
+
+*/
+
+[InitializeOnLoad]
+public class ServerUtil
+{
+    public static int saveInterval = 2;
+    public const int MAX_SAVED_SCENES = 10;
+    public static string currentFolderPath = "Assets/Scenes/";
+
+    public void Update()
+    {
+    }
+
+    public static void saveToNewScene()
+    {
+        string folderName = "Autosaved Scenes";
+        string newTimestamp = "TS-" + getTimestamp(DateTime.Now, true, true);
+
+        int tsLength = newTimestamp.Length;
+
+        string newSceneName, oldSceneName;
+        Scene newScene;
+
+        int length = EditorSceneManager.GetActiveScene().name.IndexOf("TS-"); // delimiter
+                                                                              // create the new scene
+                                                                              // if we are not currently in a scene
+
+        if (EditorSceneManager.GetActiveScene().name == "") // if we are not in a scene
+        {
+            newSceneName = "Autosaved Scene" + newTimestamp;
+            oldSceneName = "";
+
+            newScene = EditorSceneManager.NewScene(NewSceneSetup.EmptyScene, NewSceneMode.Single);
+        }
+
+        else // if we are in a scene
+        {
+            if (length < 0) // if the scene name does not contain the delimiter
+            {
+                newSceneName = "Test Scene" + " " + newTimestamp;
+
+                oldSceneName = EditorSceneManager.GetActiveScene().name;
+            }
+            // if there is an open scene
+            else
+            {
+                // Get the new scene name
+                oldSceneName = EditorSceneManager.GetActiveScene().name;
+
+                newSceneName = oldSceneName.Substring(0, length - 1) + " " + newTimestamp;
+                // got the new scene name
+            }
+            // create new scene
+            newScene = EditorSceneManager.NewScene(NewSceneSetup.EmptyScene, NewSceneMode.Additive);
+
+            // copy everything from old scene into new scene
+            EditorSceneManager.MergeScenes(EditorSceneManager.GetSceneByName(oldSceneName), newScene);
+            // copied everything from old scene into new scene
+        }
+
+        string savedScene = currentFolderPath + folderName + "/" + newSceneName + ".unity";
+        EditorSceneManager.SaveScene(newScene, savedScene, false);
+        Lightmapping.Bake();
+    }
+
+public static void checkTooManyScenes()
+    {
+        string path;
+        if (EditorSceneManager.GetActiveScene().name == "") // if we are not in a scene
+            path = "Assets/Scenes/Autosaved Scenes/"; // path to autosaved scenes
+        else // if we are
+        {
+            // get file the scene is in
+            string sceneName = EditorSceneManager.GetActiveScene().name + ".unity";
+            Debug.Log(sceneName);
+            int lim = sceneName.Length;//EditorSceneManager.GetActiveScene().name.Length;// + ".unity";
+            path = EditorSceneManager.GetActiveScene().path.Substring(0, EditorSceneManager.GetActiveScene().path.Length - lim); // take scene name out of path
+            Debug.Log(path);
+        }
+
+        DirectoryInfo levelDirectoryPath = new DirectoryInfo(path); // asset information at path
+        FileInfo[] scenesInfo = levelDirectoryPath.GetFiles();  // array of files at the path
+
+        Queue<String> sceneNames = new Queue<String>(); // queue of names in the file
+
+        foreach (FileInfo fi in scenesInfo)
+            if (fi.Name.Contains(".meta") == false) // filter out .meta files
+                sceneNames.Enqueue(fi.Name); // add appropriate file names into the queue
+
+        if (sceneNames.Count > 10) // if there are more than 10 scenes saved
+        {
+            while (sceneNames.Count > 10) // run loop while there are more than 10 scenes in folder
+            {
+                string earliestScene = sceneNames.Dequeue(); // name of earliest scene in the file
+                AssetDatabase.DeleteAsset(path + earliestScene); // delete the scene
+            }
+        }
+    }
+
+    public static String getTimestamp(DateTime val, bool date, bool time)
+    {
+        if (date && !time)
+            return val.ToString("yyyy.MM.dd");
+        else if (!date && time)
+            return val.ToString("HH.mm.ss");
+        else
+            return val.ToString("yyyy.MM.dd HH.mm.ss");
+    }
+}
